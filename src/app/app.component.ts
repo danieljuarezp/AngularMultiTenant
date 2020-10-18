6791cf60a1a5e3ed40a7c7edba98953f3015703e
Sinan NAR <sinan.nar@gmail.com>
2020-10-18 19:52:17 +1300

    running app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, OnInit } from '@angular/core';
+import { Tenant, TenantService } from './tenant/tenant.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularMultiTenant';
+
+  constructor(private tenantService: TenantService) {
+  }
+
+  @HostBinding('class.theme-client1') public client1Theme: boolean;
+  @HostBinding('class.theme-client2') public client2Theme: boolean;
+
+  ngOnInit() {
+    this.enableThemes();
+  }
+
+  enableThemes() {
+    this.client1Theme = this.tenantService.getTenant() === Tenant.CLIENT1;
+    this.client2Theme = this.tenantService.getTenant() === Tenant.CLIENT2;
+  }
+
+  get tenant() : string {
+    return this.tenantService.getTenant();
+  }
 }
